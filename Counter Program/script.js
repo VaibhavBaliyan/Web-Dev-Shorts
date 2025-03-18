acc1b67f6ddbdf52570d4675270c64413b009764
Vaibhav Baliyan <vaibhavbaliyan2002@gmail.com>
2025-03-18 11:24:19 +0530

    Initial commit

---
+++ Counter Program/script.js
@@ -0,0 +1,21 @@
+const decrement = document.getElementById("decrement");
+const reset = document.getElementById("reset");
+const increment = document.getElementById("increment");
+const count = document.getElementById("count");
+
+let countValue = 0;
+
+increment.onclick = function () {
+    countValue++;
+    count.textContent = countValue;
+}
+
+decrement.onclick = function () {
+    countValue--;
+    count.textContent = countValue;
+}
+
+reset.onclick = function () {
+    countValue = 0;
+    count.textContent = countValue;
+}
